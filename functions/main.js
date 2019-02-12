cb847cb4fe34f1f9f81ad6b247797246f90b6df4
Jaime Lopez <loama18@gmail.com>
2019-02-11 20:03:21 -0500

    correctly scrap the letters for links

--- functions/main.js
+++ functions/main.js
@@ -1,14 +1,31 @@
 const helpers = require('./helpers.js')
 
 const rp = require('request-promise')
-const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States'
+const $ = require('cheerio')
+const url = 'https://naruto.fandom.com/wiki/Category:Characters'
 
 exports.fetchData = function (req, res) {
   rp(url)
     .then(function (html) {
-      // success!
-      console.log(html)
-      helpers.result(req, res, 200, 'success', 'data fetched', html)
+    // home html is fetched
+      let alphabetShortcuts = $('ul[class=category-page__alphabet-shortcuts] > li[class=category-page__alphabet-shortcut]', html)
+
+      let list = []
+      alphabetShortcuts.find('a').each(function (index, element) {
+        let text = $(element).text()
+        if (text === 'Other') { // We want to save links, the link for 'other' in the webpage is '!'
+          text = '!'
+        }
+        if (text !== '#') {
+          list.push(text)
+        }
+      })
+
+      // list now contains all the characters we should iterate into for fetching
+
+      console.log(list)
+
+      helpers.result(req, res, 200, 'success', 'data fetched', list)
     })
     .catch(function (err) {
       // handle error
